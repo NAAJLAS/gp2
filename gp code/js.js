e60ccef106e9c5f2229730021146910fc0b5d7bb
NAAJLAS <60428257+NAAJLAS@users.noreply.github.com>
2021-02-05 23:52:01 +0300

    Add files via upload

---
+++ gp code/js.js
@@ -0,0 +1,8 @@
+
+function openNav() {
+var x = document.getElementById('sideLink');
+   if (x.style.display == 'none') {
+       x.style.display = 'block';
+   } else {
+       x.style.display = 'none';
+   } }
